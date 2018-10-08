869b60fdf045371f8478fe9124a2f9f237ea3822
Carlos R Arrivillaga <carlosra210@gmail.com>
2018-10-08 14:57:44 -0400

    ..///

--- js/calculator.js
+++ js/calculator.js
@@ -6,32 +6,61 @@ var result2 = 0
 
 const feet = 1728
 const pulgada = 2.54
+const pricePerFeet = 17
 
 document.getElementById('result').style.display = 'none';
 document.getElementById('format').style.display = 'none';
+document.getElementById('price').style.display = 'none';
+document.getElementById('errorMessage').style.display = 'none';
 
 function getResult(){
-   getLarge = document.getElementById('large').value;
-   getWidth = document.getElementById('width').value;
-   getHeight = document.getElementById('height').value;
+   var allowed = true
+   var inputL = document.getElementById('large').value;
+   var inputW = document.getElementById('width').value;
+   var inputH = document.getElementById('height').value;
 
-   var large = parseInt(getLarge);
-   // if(isNaN(large)) num = 0;
+   if (inputL == '') {
+      allowed = false;
+   }
+   if (inputW == '') {
+      allowed = false;
+   }
+   if (inputH == '') {
+      allowed = false;
+   }
 
-   var width = parseInt(getWidth);
-   // if(isNaN(width)) num = 0;
+   if(!allowed){
+      document.getElementById('errorMessage').style.display = 'block';      
+   }else{
+      document.getElementById('errorMessage').style.display = 'none';
+      
+      getLarge = document.getElementById('large').value;
+      getWidth = document.getElementById('width').value;
+      getHeight = document.getElementById('height').value;
 
-   var height = parseInt(getHeight);
-   // if(isNaN(height)) num = 0;
+      var large = parseInt(getLarge);
+      // if(isNaN(large)) num = 0;
 
-   result1 = large * width * height
-   result2 = result1 / feet
-   var final = Math.round(result2)
+      var width = parseInt(getWidth);
+      // if(isNaN(width)) num = 0;
 
-   
-   document.getElementById('result').style.display = 'block';
-   document.getElementById('format').style.display = 'block';
-   document.getElementById('result').innerHTML = final;
+      var height = parseInt(getHeight);
+      // if(isNaN(height)) num = 0;
+
+      result1 = large * width * height
+      result2 = result1 / feet
+      var final = Math.round(result2)
+
+      var precio = final * pricePerFeet
+
+      
+      document.getElementById('result').style.display = 'block';
+      document.getElementById('format').style.display = 'block';
+      document.getElementById('price').style.display = 'block';
+
+      document.getElementById('result').innerHTML = final;
+      document.getElementById('priceNumber').innerHTML ='$' + precio + ' USD';
+   }
 }
 
 function getConvertion(){
